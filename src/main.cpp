94b3979e963d433975f575c88a6e13f59048be1b
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-06-15 09:23:52 -0400

    Add parsing for response headers

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,28 @@ int main() {
   std::string requestURL{ "http://ip-api.com/json" };
   // Request the data
   auto [result, data, headers] = cURL::getData(requestURL, handle);
+
+  std::cout << "Response headers:\n";
+  for(const auto& header : headers) {
+    std::cout << header;
+  }
+
+  // TODO:
+  // Parse and validate headers
+
   // Parse the JSON
   Json::Value parsedData = JSON::parseData(data);
+
+  // TODO:
+  // Status
+  // City
+  // Region/State
+  // Zip
+  // Lat/Long
+  // IP
+
   // Process data
+  std::cout << "ResponseData:\n";
   std::cout << data;
   return 0;
 }
